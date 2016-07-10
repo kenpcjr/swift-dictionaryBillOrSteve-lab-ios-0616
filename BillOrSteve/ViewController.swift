48895674aec3f88c47a22a86c5ff4abccf4670c7
kenpcjr <kennethcooke@me.com>
2016-07-10 19:36:51 -0400

    Done.

--- BillOrSteve/ViewController.swift
+++ BillOrSteve/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+var questions = [String : [String]]()
+var currentFact = String()
+var currentOwner = String()
+var currentScore = 0
 
 class ViewController: UIViewController {
+    @IBOutlet weak var factLabel: UILabel!
+    @IBOutlet weak var scoreCounter: UILabel!
+    @IBOutlet weak var billButton: UIButton!
+    @IBOutlet weak var steveButton: UIButton!
+    
+    @IBAction func steveTapped(sender: AnyObject) {
+        checkForWin("Steve Jobs")
+    }
+    @IBAction func billTapped(sender: AnyObject) {
+        checkForWin("Bill Gates")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        createFacts()
+        showFact()
+        scoreCounter.text = "\(currentScore)"
+        
     }
-
+    
     func randomNumberFromZeroTo(number: Int) -> Int {
         return Int(arc4random_uniform(UInt32(number)))
     }
@@ -25,5 +47,57 @@ class ViewController: UIViewController {
         } else {
             return "Bill Gates"
         }
+    }
+    
+    func createFacts(){
+        let steveFacts = [
+            "He took a calligraphy course, which he says was instrumental in the future company products' attention to typography and font.",
+            
+            "Shortly after being shooed out of his company, he applied to fly on the Space Shuttle as a civilian astronaut (he was rejected) and even considered starting a computer company in the Soviet Union.",
+            
+            "He actually served as a mentor for Google founders Sergey Brin and Larry Page, even sharing some of his advisers with the Google duo.",
+            
+            "He was a pescetarian, meaning he ate no meat except for fish."]
+        
+        let billFacts = [
+            "He aimed to become a millionaire by the age of 30. However, he became a billionaire at 31.",
+            
+            "He scored 1590 (out of 1600) on his SATs.",
+            "His foundation spends more on global health each year than the United Nation's World Health Organization.",
+            
+            "The private school he attended as a child was one of the only schools in the US with a computer. The first program he ever used was a tic-tac-toe game.",
+            
+            "In 1994, he was asked by a TV interviewer if he could jump over a chair from a standing position. He promptly took the challenge and leapt over the chair like a boss."]
+        
+        questions["Steve Jobs"] = steveFacts
+        questions["Bill Gates"] = billFacts
+        
+    }
+    
+    func getRandomFact() -> (fact: String, owner: String)?{
+        var fact = ""
+        let owner = randomPerson()
+        if let factArray = questions[owner]{
+            let factUnwrapped = factArray[randomNumberFromZeroTo(factArray.count)]
+            
+            fact = factUnwrapped
+        }
+        return (fact, owner)
+    }
+    
+    func showFact(){
+        if let currentSelection = getRandomFact(){
+        currentFact = currentSelection.fact
+        currentOwner = currentSelection.owner
+            factLabel.text = currentSelection.fact
+        }
+    }
+    
+    func checkForWin(guess: String) {
+        if guess == currentOwner{
+            currentScore += 1
+            scoreCounter.text = "\(currentScore)"
+        }
+        showFact()
     }
 }
